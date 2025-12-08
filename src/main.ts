4122259e0b6ef91434448322726299ff6a3710a2
Hunter-Thompson <aatman@lottiefiles.com>
2025-12-08 21:12:11 +0530

    init

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ export class MyChart extends Chart {
       kind: 'Pod',
       metadata: {
         namespace: 'frontend',
-        name: 'nginx',
+        name: 'ngixnx',
         labels: label,
       },
       spec: {
