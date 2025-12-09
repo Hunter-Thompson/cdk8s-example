8180a4613cb3dfaf2cb23ad5e9a419b6245325fd
Hunter-Thompson <aatman@lottiefiles.com>
2025-12-09 18:59:22 +0530

    init

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ export class MyChart extends Chart {
 
     const label = { app: 'hello-k8s' };
     // set image as env IMAGE
-    const image = process.env.IMAGE 
+    const image = process.env.ARGOCD_ENV_IMAGE 
 
     // create a deployment nginx  
     new ApiObject(this, 'deployment', {
